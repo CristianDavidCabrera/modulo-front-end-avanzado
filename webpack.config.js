197edd4b66f3b02df4de5ceda2a973fecca0b393
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-05-14 18:47:33 +0100

    refactor, changed project structure, added src folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname,'main.js'),
+    mode:'development',
+    entry: path.join(__dirname,'src','main.js'),
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname,'dist'),
     },
     plugins: [
         new htmlPlugin({
-            template: 'index.html',
+            template: path.join(__dirname,'src','index.html'),
         }),
     ],
 };
