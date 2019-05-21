db197c54ee58261a454786736987eea78bbd17de
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-05-21 08:12:31 +0100

    sourceMap added to webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,21 @@ module.exports = {
         rules:[
             { 
               test:/\.scss$/,
-              use: ['style-loader','css-loader','sass-loader'] 
+              use: [
+                  'style-loader', 
+                  {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                  },
+                ], 
             },
         ],
     },
