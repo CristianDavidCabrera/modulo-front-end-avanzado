2e79779b7084b89deec6fb048703537cc3c35b7d
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-07-10 12:50:33 +0100

    added plugin: clean webpack pluging

--- webpack.production.js
+++ webpack.production.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const htmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode:'production',
@@ -25,15 +26,20 @@ module.exports = {
                 use:[
                     {
                       loader: 'file-loader',
-                      options: {},
+                      options: {
+                        name:'[name].production.[ext]',
+                      },
                     },
                 ], 
             },
         ],
     },
     plugins: [
+        new webpack.ProgressPlugin(),
         new htmlPlugin({
             template: path.join(__dirname,'src','index.html'),
         }),
+        new CleanWebpackPlugin(),
     ],
 };
+
