4cb2352a83d09d588eb14336bb3dfa1520230637
Cristian David Cabrera Cabrera <cristiancabrera.1985@gmail.com>
2019-07-10 13:26:20 +0100

    added plugin: postcss with autoprefixer

--- webpack.production.js
+++ webpack.production.js
@@ -1,45 +1,46 @@
-const path = require('path');
-const webpack = require('webpack');
-const htmlPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const htmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode:'production',
-    entry: path.join(__dirname,'src','main.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname,'dist'),
-    },
-    module:{
-        rules:[
-            { 
-              test:/\.scss$/,
-              use: ['style-loader', 'css-loader','sass-loader'],
-            },
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(jpg|png|gif)$/,
-                use:[
-                    {
-                      loader: 'file-loader',
-                      options: {
-                        name:'[name].production.[ext]',
-                      },
-                    },
-                ], 
-            },
-        ],
-    },
-    plugins: [
-        new webpack.ProgressPlugin(),
-        new htmlPlugin({
-            template: path.join(__dirname,'src','index.html'),
-        }),
-        new CleanWebpackPlugin(),
-    ],
+  mode: "production",
+  entry: path.join(__dirname, "src", "main.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader","postcss-loader", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].production.[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new htmlPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    }),
+    new MiniCssExtractPlugin(),
+  ]
 };
-
